f04c4149f0a23e51c078872462e917212742e948
Anatoly Yakovenko <anatoly@solana.com>
2022-01-05 17:02:49 -0800

    wip

--- programs/token-omnibus/src/lib.rs
+++ programs/token-omnibus/src/lib.rs
@@ -5,10 +5,20 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod token_omnibus {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(_ctx: Context<Initialize>, data: SHA256) -> ProgramResult {
         Ok(())
     }
 }
 
+type SHA256 = [u8;32];
+
+#[account]
+struct AccountSet {
+    root: SHA256,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = account_set, space = 32)]
+    pub account_set: Account<'info, AccountSet>,
+}
---
+++ programs/token-omnibus/src/state.rs
@@ -0,0 +1,3 @@
+use anchor_lang::prelude::*;
+
+
