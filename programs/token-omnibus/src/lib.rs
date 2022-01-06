c707443d4e572e44c85d8697ce8324d0d22877d1
Anatoly Yakovenko <anatoly@solana.com>
2022-01-05 17:26:34 -0800

    wip

--- programs/token-omnibus/src/lib.rs
+++ programs/token-omnibus/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
 
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
@@ -8,13 +9,29 @@ pub mod token_omnibus {
     pub fn initialize(_ctx: Context<Initialize>, _data: SHA256) -> ProgramResult {
         Ok(())
     }
+    pub fn deposit(_ctx: Context<Initialize>, _data: DepositArgs) -> ProgramResult {
+        Ok(())
+    }
 }
 
-pub type SHA256 = [u8;32];
+pub type SHA256 = [u8; 32];
 
 #[account]
 pub struct AccountSet {
     root: SHA256,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct DepositArgs {
+    ///  proof that value is zero
+    ///  Proof must start at SHA256(destination owner, amount)
+    proof_zero: [SHA256; 20],
+
+    /// amount must be delegated by the source token Account
+    amount: u64,
+
+    /// PDA(Token Mint Account, pda_bump)
+    pda_bump: u8,
 }
 
 #[derive(Accounts)]
@@ -24,4 +41,9 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    pub account_set: Account<'info, AccountSet>,
 }
