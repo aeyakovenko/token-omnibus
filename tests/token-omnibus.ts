699e000fdc4fd6d37cb1617f66902a4fe17a973a
Anatoly Yakovenko <anatoly@solana.com>
2022-01-08 09:03:39 -0800

    update

--- tests/token-omnibus.ts
+++ tests/token-omnibus.ts
@@ -3,15 +3,22 @@ import { Program } from '@project-serum/anchor';
 import { TokenOmnibus } from '../target/types/token_omnibus';
 
 describe('token-omnibus', () => {
+  const provider = anchor.Provider.env();
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider();
 
   const program = anchor.workspace.TokenOmnibus as Program<TokenOmnibus>;
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    var data = Array<byte>(32).fill(0);
+    const tx = await program.rpc.initialize(data, {
+        accounts: {
+            user: provider.wallet.publicKey,
+        },
+    });
     console.log("Your transaction signature", tx);
+    const state = await program.state.fetch();
   });
 });
